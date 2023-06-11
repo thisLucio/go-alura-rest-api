9fa035a0bd3e3dbf9df408eb36eee84ab5b375c2
thisLucio <luxusnietzsche@gmail.com>
2023-06-11 18:43:37 -0300

    First controller

--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
 
+func Home(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Home Page")
+
+}
+func HandleRequest() {
+	http.HandleFunc("/", Home)
+	log.Fatal(http.ListenAndServe(":8000", nil))
+}
 func main() {
-	fmt.Println("Iniciando estudos REST com GO")
+	fmt.Println("Iniciando o servidor Rest com Go")
+	HandleRequest()
 }
